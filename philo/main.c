f879e8621a09a45a80ae1eed94502cf2c8c04d20
jean <jean5326a@gmail.com>
2025-12-03 17:02:36 +0000

    a

--- philo/main.c
+++ philo/main.c
@@ -3,31 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jemoreir <jemoreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/01 19:39:54 by marvin            #+#    #+#             */
-/*   Updated: 2025/12/01 19:39:54 by marvin           ###   ########.fr       */
+/*   Created: 2025/12/03 17:01:31 by jemoreir          #+#    #+#             */
+/*   Updated: 2025/12/03 17:01:31 by jemoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main()
+int main(int argc, char **argv)
 {
-    t_data data;
+	t_data data;
 
-    data.n_philos = 5;
-    data.time_to_die = 800;
-    data.time_to_eat = 300;
-    data.time_to_sleep = 500;
-    data.must_eat = -1;
-
-    if (!ft_init_inside_data(&data))
-        return (printf("Erro init inside data\n"));
-    if (!ft_init_philos(&data))
-        return (printf("Erro init philos\n"));
-    if (!ft_start_simulation(&data))
-        return (printf("Erro start simulation\n"));
-    ft_destroy_all(&data);
-    return (0);
+	if (!ft_init_data(&data, argc, argv))
+		return (printf("Erro init data\n"));
+	if (!ft_init_inside_data(&data))
+		return (printf("Erro init inside data\n"));
+	if (!ft_init_mutexes(&data))
+		return (printf("Erro init mutexes\n"));
+	if (!ft_init_philos(&data))
+		return (printf("Erro init philos\n"));
+	if (!ft_start_simulation(&data))
+		return (printf("Erro start simulation\n"));
+	ft_destroy_all(&data);
+	return (0);
 }
